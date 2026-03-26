3462f1951e5f433d079c27e7b22e9061080e9801
sionhyeop <l50227697@gmail.com>
2026-03-26 14:46:33 +0900

    첫 커밋

---
+++ Project1/실습3.c
@@ -0,0 +1,53 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//void Lab7(void) {
+//
+//	int x = 3;
+//	int y = -3;
+//
+//	printf("x=%08X\n", x);
+//	printf("y=%08X\n", y);
+//	printf("x+y=%08X\n", x + y);
+//}
+//
+//void Lab8(void) {
+//	char code1 = 'A';
+//	char code2 = 65;
+//
+//	printf("code1= %c\n", code1);
+//	printf("code2=%c\n", code2);
+//}
+//
+//void Lab9(void) {
+//	char code = 'A';
+//	printf("%d %d %d \n", code, code + 1, code + 2);
+//	printf("%c %c %c \n", code, code + 1, code + 2);
+//}
+//
+//void Lab10(void) {
+//	char beep = '\a';
+//	printf("%c", beep);
+//}
+//
+//void Lab11(void) {
+//
+//	int x, y, money;
+//	printf("원금과 이자율(%%)을 정수 형태로 띄워서 입력해주시오 : ");
+//	scanf_s("%d %d", &x, &y);
+//
+//	money = x + x * y / 100; //1년
+//	money = money + money * y / 100; //2년
+//
+//	printf("result : %d", money);
+//}
+//
+//
+//void Lab12(void) {
+//
+//	char x;
+//	printf("문자를 입력하시오:");
+//	scanf_s("%c", &x, 1);
+//
+//	printf("ASCII코드 : %d", x);
+//}
